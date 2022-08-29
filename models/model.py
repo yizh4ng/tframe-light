5d2f0fef5e808b14d35b5e7bea7658da077ec44e
unknown <50898990+1155107756@users.noreply.github.com>
2022-08-29 20:15:38 +0800

    It is hard to understand why dataset is a Noear

--- models/model.py
+++ models/model.py
@@ -1,7 +1,9 @@
 import numpy as np
 from tensorflow import keras
 import tensorflow as tf
-from tframe import console
+from tframe import console, DataSet
+from lambo.gui.vinci.vinci import DaVinci
+
 
 class Model(object):
   def __init__(self,loss, metrics, net, name='DefaultModel'):
@@ -46,4 +48,39 @@ class Model(object):
     self.keras_model = keras.Model(inputs=input, outputs=output,
                                    name=self.net.name)
 
+  def show_feauture_maps(self, dataset:DataSet):
+    assert isinstance(self.keras_model, tf.keras.Model)
+    feature_maps = []
+    titles = []
+    for layer in self.keras_model.layers:
+      # dealing with images
+      if len(layer.output.shape) == 4:
+        for i in range(layer.output.shape[-1]):
+          feature_maps.append(
+            tf.reduce_sum(
+              tf.slice(
+                layer.output, [0,0,0,i], [-1,-1,-1,1]),
+              axis=(0, -1))
+          )
+          titles.append('{} {}'.format(layer.name, i))
+      # dealing with dense layers
+      elif len(layer.output.shape) == 2:
+        feature_maps.append(layer.output)
+        titles.append('{}'.format(layer.name))
+      else:
+        console.show_status(
+          'Unknow layer types {} or shapes {}'.format(type(layer),
+                                                      layer.output.shape))
+    da = DaVinci()
+    da.objects = dataset.features
 
+    model_temp = tf.keras.Model(inputs=self.keras_model.input,
+                                outputs=feature_maps)
+
+    def _show_feature_map(x, feature_index):
+      da.imshow(model_temp(np.expand_dims(x, 0))[feature_index], title=titles[feature_index])
+
+    da.add_plotter(da.imshow)
+    for i in range(len(feature_maps)):
+      da.add_plotter(lambda x, _i = i: _show_feature_map(x, _i))
+    da.show()
